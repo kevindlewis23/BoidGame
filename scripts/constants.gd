a1d573e5c6f1f9dc7787230fcdf646ef7a01100e
Kevin Lewis <kevindlewis23@gmail.com>
2025-08-07 18:49:53 -0400

    Move game data to user:// where it belongs

--- scripts/constants.gd
+++ scripts/constants.gd
@@ -7,6 +7,6 @@ static var global_aabb = Rect2(0, 0, WIDTH, HEIGHT)
 static var player_boid_color = Color(Color.hex(0xabf3feff))
 static var predator_boid_color = Color(Color.hex(0xff0000ff))
 static var predator_player_boid_color = Color(Color.hex(0xff9900ff))
-static var level_creator_tmp_file_path = "res://game_data/level_creator_file_tmp.json"
+static var level_creator_tmp_file_path = "user://level_creator_file_tmp.json"
 
-static var passed_levels_file_path = "res://game_data/passed_levels.json"
+static var passed_levels_file_path = "user://passed_levels.json"
